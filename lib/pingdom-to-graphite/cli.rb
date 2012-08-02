b05ff803f7d86258cb5a8b001181f364d02768dd
Lew Goettner <lew@goettner.net>
2012-08-02 16:39:18 -0400

    Parse pingdom creds and return the list of checks.

--- lib/pingdom-to-graphite/cli.rb
+++ lib/pingdom-to-graphite/cli.rb
@@ -18,26 +18,33 @@ class PingdomToGraphite::CLI < Thor
                 :aliases  => "-s",
                 :default  => "~/.p2g/state.json"
 
+
   desc "init", "Create an empty config JSON file if missing."
   def init
     config_path = File.expand_path(options.config)
+
     if File.exists?(config_path)
       error("Config file already exists. (#{options.config})")
     else
+
+      # Make sure we have a directory to put the config in
       unless File.directory?(File.dirname(config_path))
         FileUtils.mkdir_p(File.dirname(config_path), :mode => 0700)
       end
+
       settings = {
-        "username"  => "REPLACE_ME",
-        "password"  => "REPLACE_ME",
-        "key"       => "REPLACE_ME"
+        "username"  => "YOUR_USERNAME",
+        "password"  => "YOUR_PASSWORD",
+        "key"       => "YOUR_API_KEY"
       }
       File.open(File.expand_path(options.config),"w",0600) do |f|
         f.write(JSON.pretty_generate(settings))
       end
+
     end
 
   end
+
 
   desc "list", "List all your available Pingdom checks."
   def list
@@ -48,18 +55,26 @@ class PingdomToGraphite::CLI < Thor
     puts "Password: #{@password}"
     puts "Key: #{@key}"
     puts "State: #{options.state}"
-    
+    datapull = PingdomToGraphite::DataPull.new(@username, @password, @key)
+    datapull.get_checks.each do |check|
+      puts "#{check.name} (#{check.id}) - #{check.status}"
+    end
   end
 
   private
 
   def load_config!
-    unless File.exists?(File.expand_path(options.config)) 
+    config_file = File.expand_path(options.config)
+    unless File.exists?(config_file) 
       error("Missing config file (#{options.config})")
       exit
     end
 
-    config = JSON::parse(options.config);
+    config = JSON::parse(File.read(config_file));
+
+    @username = config["username"]
+    @password = config["password"]
+    @key = config["key"]
 
   end
 
